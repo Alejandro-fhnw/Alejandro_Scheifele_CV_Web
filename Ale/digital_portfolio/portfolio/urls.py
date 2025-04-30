ffafa1a34129b60dcb586e81b888058b4b7a4f7d
Alejandro-fhnw <alejandro.scheifeledelrio@outlook.com>
2025-04-30 15:26:53 +0200

    #

---
+++ Ale/digital_portfolio/portfolio/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('skills/', views.skills, name='skills'),
+    path('work/', views.work, name='work'),
+    path('projects/', views.projects, name='projects'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('contact/', views.contact, name='contact'),
+]
